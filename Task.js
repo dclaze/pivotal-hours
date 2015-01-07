af0e96ef72a66f6e44c3c6561a3f76f46519a90c
Chris Mondok <chris.mondok@gmail.com>
2015-01-07 18:40:42 -0500

    Allow for tasks to save their completion state

--- Task.js
+++ Task.js
@@ -1,18 +1,38 @@
 angular.module('PivotalApp').factory('Task', ['$resource', function($resource) {
-    var Task = $resource('projects/:project_id/stories/:story_id/tasks', {}, {
+    var Task = $resource('projects/:project_id/stories/:story_id/tasks/:id', {
+            id:'@id',
+            story_id:'@story_id'
+        }, {
         query: {
             isArray: true,
             transformResponse: function(response) {
                 var tasks = JSON.parse(response);
-                tasks.forEach(function(t) {
-                    t.hours = parseTaskHours(t);
-                });
+                tasks.forEach(initTask);
                 return tasks;
+            }
+        },
+        saveComplete: {
+            method: 'PUT',
+            transformRequest: function(task) {
+                return JSON.stringify({
+                    id: task.id,
+                    story_id: task.story_id,
+                    complete: task.complete
+                });
+            },
+            transformResponse: function(response) {
+                var task = JSON.parse(response);
+                initTask(task);
+                return task;
             }
         }
     });
 
-    function parseTaskHours(task) {
+    function initTask(task) {
+        task.hours = parseHours(task);
+    }
+
+    function parseHours(task) {
         var match = task.description.match(/\[(\s*\d+\s*)\]/);
         if (match) {
             var value = match[1].trim();
