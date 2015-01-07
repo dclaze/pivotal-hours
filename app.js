345c2b0a82e3b4edd7e2e52e5f4c5d2f02d9e6b0
Chris Mondok <cmondok@marathondata.com>
2015-01-07 18:03:31 -0500

    Switch to ngResource

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
-angular.module('PivotalApp', []);
+angular.module('PivotalApp', ['ngResource']);
 
 angular.module('PivotalApp').config(['$httpProvider', function($httpProvider) {
     $httpProvider.interceptors.push(function() {
         var baseUrl = "https://www.pivotaltracker.com/services/v5/";
         return {
             request: function(request) {
-                request.url = baseUrl + request.url
+                request.url = baseUrl + request.url;
                 return request;
             }
         };
     });
 }]);
 
-angular.module('PivotalApp').controller('Main', ['$scope', '$http', function($scope, $http) {
+angular.module('PivotalApp').controller('Main', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
     $scope.tasksOnly = false;
     var loadMembers = function() {
         $http.get('projects/' + $scope.projectId + '/memberships', {}).success(function(response) {
             $scope.members = response;
         });
     };
+
+    var Story = $resource('projects/:projectId/stories', {}, {
+        query: {
+            isArray: true,
+            transformResponse: function(response) {
+                var stories = JSON.parse(response);
+                stories.forEach(function(story) {
+                    story.tasks = Task.query({projectId: $scope.projectId, storyId: story.id});
+                });
+                return stories;
+            }
+        }
+    });
+
+    Story.prototype.getCompletedTaskTotal = function() {
+        return this.tasks
+            .filter(function(t) {return t.complete;})
+            .map(function(t) {return t.hours || 0;})
+            .reduce(function(a, b) {return a + b;}, 0);
+    };
+
+    Story.prototype.getTaskTotal = function() {
+        return this.tasks
+            .map(function(t) {return t.hours || 0;})
+            .reduce(function(a, b) {return a + b;}, 0);
+    };
+
+    var Task = $resource('projects/:projectId/stories/:storyId/tasks', {}, {
+        query: {
+            isArray: true,
+            transformResponse: function(response) {
+                var tasks = JSON.parse(response);
+                tasks.forEach(function(t) {
+                    t.hours = parseTaskHours(t);
+                });
+                return tasks;
+            }
+        }
+    });
 
     var init = function() {
         $scope.token = localStorage.getItem("token");
@@ -38,67 +77,26 @@ angular.module('PivotalApp').controller('Main', ['$scope', '$http', function($sc
         localStorage.setItem("projectId", projectId);
     };
 
-    $scope.loadWork = function() {
-        $http.get('projects/' + $scope.projectId + '/stories', {
-            params: {
-                filter: 'owner:' + $scope.member.person.id
-            }
-        }).success(function(response) {
-            $scope.stories = response;
-            $scope.stories.forEach(function(story) {
-                getTasks(story).success(function(tasks) {
-                    story.tasks = tasks.map(function(t) {
-                        t.hours = parseTaskHours(t);
-                        return t;
-                    });
-                });
+    $scope.$watch('member.person.id', function(personId) {
+        if(personId)
+            $scope.stories = Story.query({
+                projectId: $scope.projectId,
+                filter: "owner:"+$scope.member.person.id
             });
-        });
+        else
+            $scope.stories = [];
+    });
+
+    $scope.getSumOfCompletedHours = function() {
+        return ($scope.stories || [])
+            .map(function(s) { return s.getCompletedTaskTotal(); })
+            .reduce(function(a, b) {return a + b;}, 0);
     };
 
-    var getTasks = function(story) {
-        return $http.get('projects/' + $scope.projectId + '/stories/' + story.id + '/tasks');
-    };
-
-    $scope.$watch('stories', function(stories) {
-        if (!stories)
-            return;
-        var tasks = stories.map(function(story) {
-            return story.tasks;
-        }).filter(function(task) {
-            return typeof(task) != "undefined";
-        });
-        tasks = tasks.reduce(function(a, b) {
-            return a.concat(b);
-        }, []);
-        $scope.completedTaskCount = $scope.getCompletedTaskTotal(tasks);
-        $scope.totalTaskCount = $scope.getTaskTotal(tasks);
-    }, true);
-
-    $scope.getCompletedTaskTotal = function(tasks) {
-        if (!tasks)
-            return;
-
-        return tasks.map(function(t) {
-            return t.complete ? t.hours : 0;
-        }).filter(function(t) {
-            return t != null;
-        }).reduce(function(a, b) {
-            return a + b;
-        }, 0);
-    };
-
-    $scope.getTaskTotal = function(tasks) {
-        if (!tasks)
-            return;
-
-        return tasks.map(function(t) {
-            return t.hours;
-        }).filter(function(t) {
-            return t != null;
-        }).reduce(function(a, b) {
-            return a + b;
-        }, 0);
+    $scope.getTotalHours = function() {
+        return ($scope.stories || [])
+            .map(function(s) {return s.getTaskTotal(); })
+            .reduce(function(a, b) {return a + b;}, 0);
     };
 
     var parseTaskHours = function(task) {
